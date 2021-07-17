44c52382d765a6c476aca8af9f3cf3251aa7c54b
Alexandra <alexandra15dem@gmail.com>
2021-07-17 20:31:59 +0300

    Lesson 4. Working Adapter

---
+++ app/src/main/java/com/example/myapplication/MovieAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import java.security.AccessControlContext
+
+class MoviesAdapter(
+    context: Context,
+    private val moviesList: List<MovieDto>
+): RecyclerView.Adapter<MoviesAdapter.MoviesViewHolder>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+
+    class MoviesViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView){
+
+        private val title: TextView = itemView.findViewById(R.id.film_title)
+        private val description: TextView =  itemView.findViewById(R.id.film_description)
+//        private val rateScore:
+        private val ageRestriction: TextView = itemView.findViewById(R.id.age_restriction)
+        private val poster: ImageView = itemView.findViewById(R.id.film_poster)
+
+        fun bind(movie: MovieDto){
+            title.text = movie.title
+            description.text = movie.description
+            ageRestriction.text = movie.ageRestriction.toString() + "+"
+            poster.load(movie.imageUrl)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesAdapter.MoviesViewHolder {
+        return MoviesViewHolder(inflater.inflate(R.layout.item_movie, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: MoviesAdapter.MoviesViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    private fun getItem(position: Int): MovieDto = moviesList[position]
+
+    override fun getItemCount(): Int = moviesList.size
+}
