61c080d3b160d8db39b652f68c20f4f714522133
Alexandra <alexandra15dem@gmail.com>
2021-07-22 23:23:41 +0300

    Check before making fragments

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,20 +2,12 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.DTOs.MovieDto
-import com.example.myapplication.DataSources.MoviesDataSourceImpl
-import com.example.myapplication.Models.MoviesModel
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var moviesModel: MoviesModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_movie_details)
+        setContentView(R.layout.activity_main)
     }
 
 }
